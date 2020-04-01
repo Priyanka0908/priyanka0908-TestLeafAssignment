33f95b8b371c8e547dfd6e2e200ab0d6e9f7c11a
Priyanka0908 <62899588+Priyanka0908@users.noreply.github.com>
2020-04-01 15:45:34 +0530

    Week1.Day2 Assignment_TestLeaf Validation

---
+++ java/week1/day1/java/week1/day2/LeafTapsLogin.java
@@ -0,0 +1,41 @@
+package week1.day2;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LeafTapsLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ChromeDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		WebElement eleUsername = driver.findElementById("username");
+		eleUsername.sendKeys("DemoSalesmanager");
+		WebElement elePassword = driver.findElementByName("PASSWORD");
+		elePassword.sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.findElementByLinkText("CRM/SFA").click();
+		driver.findElementByLinkText("Create Lead").click();
+		driver.findElementById("createLeadForm_companyName").sendKeys("Cognizant");
+		driver.findElementById("createLeadForm_firstName").sendKeys("Priyanka");
+		driver.findElementById("createLeadForm_lastName").sendKeys("Dubey");
+		driver.findElementByName("submitButton").click();
+		String viewLeadtitle = driver.getTitle();
+		System.out.println(viewLeadtitle);
+		WebElement firstName = driver.findElementById("viewLead_firstName_sp");
+		String inputName = firstName.getText();
+		System.out.println(inputName);
+		String expectedName = "Priyanka";
+		if (inputName.equalsIgnoreCase(expectedName))
+				{
+			System.out.println("Result is verified");
+		}
+		else {
+			System.out.println("Result cannot be verified");
+		}
+		driver.close();
+	}
+
+}
